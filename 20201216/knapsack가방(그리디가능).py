6f0a8b984f6d846e4d7eaa944349c331bed024fe
brightface <brightface@hanmail.net>
2020-12-17 04:04:35 +0900

    빼면서 하는거라 바텀업이다. 배열순서대로
    작은무게나 무슨 무게든 상관없이. (욕심쟁이가 아니다.) 단지 조건이 가치가 더 많이 나가면 바꾸게 된다.

---
+++ 20201216/knapsack가방(그리디가능).py
@@ -0,0 +1,23 @@
+import sys
+#sys.stdin = open("input.txt")
+
+n , max_weight = map(int,input().split())
+# print(n,max_weight)
+info = [list(map(int,input().split())) for i in range(n)]
+
+#j까지 담을수 있는 최대 가치
+dy=[0]*(max_weight+1)
+#print(dy)
+
+#top down 방식 인가 bottom up 이나 top down 인것 같 ㄴㄴ bottom up
+#0부터 채운다.
+#2중포문으로 돌려야 하나?
+#빼야하는데
+for i in range(n):
+    for j in range(info[i][0],max_weight+1):
+        #if i-w >= 0:
+            dy[j] = max(dy[j-info[i][0]]+info[i][1],dy[j])
+        #else:
+         #   break
+#print(dy)
+print(max(dy))
