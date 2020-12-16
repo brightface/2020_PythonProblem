0e88edb6c379373586079e14bd4222ba6ed90ebf
brightface <brightface@hanmail.net>
2020-12-16 19:31:40 +0900

    bottomup,topdown(메모리제이션)

---
+++ 20201216/알리바바40인의도둑.py
@@ -0,0 +1,15 @@
+import sys
+#sys.stdin = open("input.txt")
+n = int(input())
+arr = [list(map(int,input().split())) for i in range(n)]
+dy = [[0]*(n) for i in range(n)]
+dy[0][0] = arr[0][0]
+for i in range(n):
+    dy[0][i] = dy[0][i-1]+arr[0][i]
+    dy[i][0] = dy[i-1][0]+arr[i][0]
+for i in range(1,n):
+    for j in range(1,n):
+        dy[i][j] = min(dy[i-1][j], dy[i][j-1]) + arr[i][j]
+
+#print(dy)
+print(dy[n-1][n-1])
